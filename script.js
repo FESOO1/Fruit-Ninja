9c8b6ae80af54c45eb5d2c5be97d04ced985a4b0
FES001 <firdavs7794948@gmail.com>
2025-02-10 21:50:51 +0500

    Working on it.

--- script.js
+++ script.js
@@ -2,14 +2,14 @@ const ballContainer = document.querySelector('.container');
 const levelText = document.getElementById('levelText');
 const scoreText = document.getElementById('scoreText');
 const healthText = document.getElementById('healthText');
-
+const aimCursor = document.querySelector('.aim');
 
 // GAME
 let game = {
     level: {
         levelCounter: 0,
-        levelNumber: [1,2,3],
-        levelDifficulty: [10,20,30],
+        levelNumber: [1,2,3,4,5,6,7,8,9],
+        levelDifficulty: [5,10,15,20,25,30,35,40,45],
     },
     score: 0,
     health: 3,
@@ -40,14 +40,24 @@ let loopInterval;
 function creatingElements() {
     for (let i = 0; i < levelDifficulty(); i++) {
         let intervalBall;
+
+        // BALL
         const ball = document.createElement('div');
         ball.classList.add('ball');
         ball.style.top = `-${randomDropPosition(150)}px`;
         ball.style.left = `${randomDropPosition((window.innerWidth - 40))}px`;
-        ball.style.backgroundColor = `rgb(${randomColor(255)}, ${randomColor(255)}, ${randomColor(255)})`;
-    
+        /* ball.style.backgroundColor = `rgb(${randomColor(255)}, ${randomColor(255)}, ${randomColor(255)})`; */
+
+        const animationRandomTime = randomDropPosition(20);
+        const animationTime = animationRandomTime < 9 ? 10 : animationRandomTime;
+        ball.style.animation = `${animationTime}s ease forwards ball-animation`;
+        
         ballContainer.appendChild(ball);
-    
+
+        // AUDIO
+        const audio = document.createElement('audio');
+        audio.src = './assets/sound-effect-2.mp3';
+
         // CATCHING THE BALL
         ball.addEventListener('click', () => {
             // ADDING POINTS
@@ -55,16 +65,38 @@ function creatingElements() {
             scoreText.textContent = game.score;
 
             // ADDING THE CAUGHT CLASS AND REMOVING THE BALLL ELEMENT AFTER 100 MS
-            ball.classList.add('ball-caught');
+            ballContainer.removeChild(ball);
+
+            // MAKING A SOUND
+            audio.play();
+        });
+
+
+        // CATCHING THE BALL
+        ballContainer.addEventListener('mousedown', () => {
+            window.addEventListener('mousemove', catchTheBall);
+            window.addEventListener('mouseup', () => window.removeEventListener('mousemove', catchTheBall));
         });
     
+        function catchTheBall(e) {
+            if (e.target === ball) {
+                // ADDING POINTS
+                game.score += 10;
+                scoreText.textContent = game.score;
+
+                // ADDING THE CAUGHT CLASS AND REMOVING THE BALLL ELEMENT AFTER 100 MS
+                ballContainer.removeChild(ball);
+
+                // MAKING A SOUND
+                audio.play();
+            };
+        };
     
         // CHECKING FOR THE BALLS THAT ARE NOT CAUGHT
         intervalBall = setInterval(() => {
             if (ball.offsetTop === window.innerHeight) {
                 game.health--;
                 healthText.textContent = game.health;
-                clearInterval(intervalBall);
     
                 // REMOVING THE ELEMENTS
                 ballContainer.removeChild(ball);
@@ -89,3 +121,10 @@ function creatingElements() {
 };
 
 creatingElements();
+
+// 
+
+document.addEventListener('mousemove', e => {
+    aimCursor.style.top = (e.clientY - (aimCursor.offsetHeight / 2)) + 'px';
+    aimCursor.style.left = (e.clientX - (aimCursor.offsetWidth / 2)) + 'px';
+});
