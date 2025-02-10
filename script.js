670a2f41f533ce2f01233d9ec9f787cf37172e45
FES001 <firdavs7794948@gmail.com>
2025-02-10 20:50:10 +0500

    Working on it.

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
 const ballContainer = document.querySelector('.container');
+const levelText = document.getElementById('levelText');
+const scoreText = document.getElementById('scoreText');
+const healthText = document.getElementById('healthText');
+
+
+// GAME
+let game = {
+    level: {
+        levelCounter: 0,
+        levelNumber: [1,2,3],
+        levelDifficulty: [10,20,30],
+    },
+    score: 0,
+    health: 3,
+};
 
 // RANDOM BACKGROUND COLOR
 
@@ -12,11 +27,21 @@ function randomDropPosition(dropPosition) {
     return Math.floor(Math.random() * dropPosition);
 };
 
+// 
+
+function levelDifficulty() {
+    return game.level.levelDifficulty[game.level.levelCounter];
+};
+
 // CREATING ELEMENTS
 
-for (let i = 0; i < 10; i++) {
+let loopInterval;
+
+for (let i = 0; i < levelDifficulty(); i++) {
+    let intervalBall;
     const ball = document.createElement('div');
     ball.classList.add('ball');
+    ball.style.top = `-${randomDropPosition(150)}px`;
     ball.style.left = `${randomDropPosition((window.innerWidth - 40))}px`;
     ball.style.backgroundColor = `rgb(${randomColor(255)}, ${randomColor(255)}, ${randomColor(255)})`;
 
@@ -25,5 +50,26 @@ for (let i = 0; i < 10; i++) {
     // CATCHING THE BALL
     ball.addEventListener('click', () => {
         ballContainer.removeChild(ball);
+
+        // ADDING POINTS
+        game.score += 10;
+        scoreText.textContent = game.score;
     });
+
+
+    // CHECKING FOR THE BALLS THAT ARE NOT CAUGHT
+    intervalBall = setInterval(() => {
+        if (ball.offsetTop === window.innerHeight) {
+            game.health--;
+            healthText.textContent = game.health;
+            clearInterval(intervalBall);
+
+            // REMOVING THE ELEMENTS
+            ballContainer.removeChild(ball);
+
+            // STARTING THE NEXT LEVEL
+            game.level.levelCounter++;
+            levelDifficulty();
+        };
+    }, 1);
 };
