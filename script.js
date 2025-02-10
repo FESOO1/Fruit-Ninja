35537a02d95bcc4dfe797bfbae63f6c02f93443b
FES001 <firdavs7794948@gmail.com>
2025-02-10 21:06:40 +0500

    Working on it.

--- script.js
+++ script.js
@@ -37,39 +37,55 @@ function levelDifficulty() {
 
 let loopInterval;
 
-for (let i = 0; i < levelDifficulty(); i++) {
-    let intervalBall;
-    const ball = document.createElement('div');
-    ball.classList.add('ball');
-    ball.style.top = `-${randomDropPosition(150)}px`;
-    ball.style.left = `${randomDropPosition((window.innerWidth - 40))}px`;
-    ball.style.backgroundColor = `rgb(${randomColor(255)}, ${randomColor(255)}, ${randomColor(255)})`;
+function creatingElements() {
+    for (let i = 0; i < levelDifficulty(); i++) {
+        let intervalBall;
+        const ball = document.createElement('div');
+        ball.classList.add('ball');
+        ball.style.top = `-${randomDropPosition(150)}px`;
+        ball.style.left = `${randomDropPosition((window.innerWidth - 40))}px`;
+        ball.style.backgroundColor = `rgb(${randomColor(255)}, ${randomColor(255)}, ${randomColor(255)})`;
+    
+        ballContainer.appendChild(ball);
+    
+        // CATCHING THE BALL
+        ball.addEventListener('click', () => {
+            // ADDING POINTS
+            game.score += 10;
+            scoreText.textContent = game.score;
 
-    ballContainer.appendChild(ball);
+            // ADDING THE CAUGHT CLASS AND REMOVING THE BALLL ELEMENT AFTER 100 MS
+            ball.classList.add('ball-caught');
+        });
+    
+    
+        // CHECKING FOR THE BALLS THAT ARE NOT CAUGHT
+        intervalBall = setInterval(() => {
+            if (ball.offsetTop === window.innerHeight) {
+                game.health--;
+                healthText.textContent = game.health;
+                clearInterval(intervalBall);
+    
+                // REMOVING THE ELEMENTS
+                ballContainer.removeChild(ball);
 
-    // CATCHING THE BALL
-    ball.addEventListener('click', () => {
-        ballContainer.removeChild(ball);
+            };
+            
+            if (ballContainer.childElementCount === 0) {
+                // GOING TO THE NEXT LEVEL
+                if (game.level.levelCounter < game.level.levelDifficulty.length) {
+                    game.level.levelCounter++;
+                    creatingElements();
+                    levelText.textContent = game.level.levelNumber[game.level.levelCounter];
+                } else {
+                    levelText.textContent = 3;
+                    console.log('No other levels');
+                };
 
-        // ADDING POINTS
-        game.score += 10;
-        scoreText.textContent = game.score;
-    });
-
-
-    // CHECKING FOR THE BALLS THAT ARE NOT CAUGHT
-    intervalBall = setInterval(() => {
-        if (ball.offsetTop === window.innerHeight) {
-            game.health--;
-            healthText.textContent = game.health;
-            clearInterval(intervalBall);
-
-            // REMOVING THE ELEMENTS
-            ballContainer.removeChild(ball);
-
-            // STARTING THE NEXT LEVEL
-            game.level.levelCounter++;
-            levelDifficulty();
-        };
-    }, 1);
+                clearInterval(intervalBall);
+            };
+        }, 1);
+    };
 };
+
+creatingElements();
